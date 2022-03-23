0e0049d7a5db3cd02be79029632255f5d5afb6d9
ASTER18K <donhon05@gmail.com>
2022-03-23 19:51:16 +0900

    Create status-logger.sh

---
+++ status-logger.sh
@@ -0,0 +1,30 @@
+Y=$(date +%Y)
+M=$(date +%m)
+D=$(date +%d)
+Hr=$(date +%H)
+Mn=$(date +%M)
+Sc=$(date +%S)
+ 
+#Ym=$Y-$M
+Ymd=$Y-$M-$D
+YmdHMS=$Y-$M-$D--$Hr:$Mn:$Sc
+ 
+GitRep="Seoul-Server-Status-log"
+ 
+HomeDir="/home/aster1sk"
+GitDir="$HomeDir/Seoul-Server-Status-log"
+FileDir="$HomeDir/Seoul-Server-Status-log/$Ymd"
+Diskuplog="$YmdHMS"-log.txt
+ 
+mkdir -p $FileDir
+ 
+echo "### $YmdHMS Status" >> $FileDir/$Diskuplog
+echo -----UPTIME----- >> $FileDir/$Diskuplog
+uptime >> $FileDir/$Diskuplog
+echo -----DISK LOG----- >> $FileDir/$Diskuplog
+df -h >> $FileDir/$Diskuplog
+ 
+cd $GitDir
+git add .
+git commit -m "commit $Diskuplog"
+git push origin master
